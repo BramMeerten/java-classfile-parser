7fab09bd95bbef92c0df18c0c065892210c92a7c
BramMeerten <bram.mee@gmail.com>
2023-11-19 14:34:54 +0100

    Read magic and version into struct

--- main.rs
+++ main.rs
@@ -1,27 +1,45 @@
-// use std::fs;
-// use std::io;
-use std::{fs::File, io::{Read, Seek, SeekFrom}};
+use std::fs::File;
+use std::io::Read;
+use std::usize;
 
+#[derive(Debug)]
+struct Class {
+    magic: u32,
+    minor_version: u16,
+    major_version: u16
+}
 
 fn main() {
-    let mut input = File::open("./java/Main.class")
-        .expect("Failed to open Main.class");
-    input.seek(SeekFrom::Start(0))
-        .expect("Could not seek");
-    
-    let mut x = input.take(8);
-    let mut magic = [0;4];
-    x.read_exact(&mut magic)
-        .expect("SHIT");
-    println!("Magic number: {:x?}", magic);
+    let bytes = open_file("./java/Main.class");
+    let magic = read_u32(&bytes, 0);
+    let minor = read_u16(&bytes, 4);
+    let major = read_u16(&bytes, 6);
 
-    let mut minor = [0,2];
-    x.read_exact(&mut minor)
-        .expect("SHIT");
-    println!("Minor version: {:x?}", minor);
+    let class = Class {
+        magic,
+        minor_version: minor,
+        major_version: major
+    };
 
-    let mut major = [0,2];
-    x.read_exact(&mut major)
-        .expect("SHIT");
-    println!("Major version: {:x?}", major);
+    println!("{:#02x?}", class);
+}
+
+fn open_file(file: &str) -> Vec<u8> {
+    let mut input = File::open(file).unwrap();
+    let mut buffer = Vec::new();
+    input.read_to_end(&mut buffer).unwrap();
+
+    buffer
+}
+
+fn read_u32(bytes: &Vec<u8>, pos: usize) -> u32 {
+    return ((bytes[pos] as u32) << 24) |
+        ((bytes[pos + 1] as u32) << 16) |
+        ((bytes[pos + 2] as u32) << 8) |
+        (bytes[pos + 3] as u32);
+}
+
+fn read_u16(bytes: &Vec<u8>, pos: usize) -> u16 {
+    return ((bytes[pos] as u16) << 8) |
+        (bytes[pos + 1] as u16);
 }
